7cd53c576b08928451950f3fa98ee4615bf36ced
abishekarjun98 <raviram19472348@gmail.com>
2020-08-25 11:44:40 +0530

    added sample questions

---
+++ questions.js
@@ -0,0 +1,29 @@
+
+
+questions=[{
+id:"1",
+question:"what is this?",
+opt_1:"beeee",
+opt_2:"bba",
+opt_3:"faaa",
+correct:"1"
+},
+
+{
+id:"2",
+question:"what is that?",
+opt_1:"qqqq",
+opt_2:"wwwww",
+opt_3:"eeee",
+correct:"2"
+}
+
+
+
+];
+
+
+
+
+	
+	
